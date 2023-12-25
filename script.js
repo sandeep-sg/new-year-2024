d790669ccba942b16a6877abc1a26991a4c12030
sandeep <gatuamsandeep2021@gmail.com>
2023-12-25 18:51:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let wish = document.querySelector("h1");
+let name = prompt("Enter your name :").toUpperCase();
+while (name == "") {
+  name = prompt("Enter your name :");
+}
+
+setInterval(() => {
+  let currentTime = new Date();
+  let newyear2024 = "1 jan 2024 12:00:00 AM";
+  let endTime = new Date(newyear2024);
+  let differenceTime = (endTime - currentTime) / 1000;
+  let days = Math.floor(differenceTime / 60 / 60 / 24);
+  let hours = Math.floor((differenceTime / 60 / 60) % 24);
+  let minutes = Math.floor((differenceTime / 60) % 60);
+  let seconds = Math.floor(differenceTime % 60);
+  wish.innerHTML = `
+    <span>${name}</span> </br> Happy New Year to you from me </br> ${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds ago`;
+}, 1000);
